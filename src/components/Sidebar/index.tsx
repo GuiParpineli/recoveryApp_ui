3baea1aeb7d8212e019ef7ef07975a3f19e60e89
Gui Parpineli <guiparpineli@GuiPC>
2022-12-31 18:09:56 -0300

    aside ADD: conf button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,7 +3,42 @@ import './style.scss'
 export function Sidebar() {
     return (
         <div className="container-sidebar">
+            <ul className="menu-side-container">
+                <div className="menu-list">
+                    <li>
+                        <span className="material-symbols-outlined">Home </span>
+                        <p>Home</p>
+                    </li>
+                </div>   <div className="menu-list">
+                    <li>
+                        <span className="material-symbols-outlined">Home </span>
+                        <p>Home</p>
+                    </li>
+                </div>   <div className="menu-list">
+                    <li>
+                        <span className="material-symbols-outlined">Home </span>
+                        <p>Home</p>
+                    </li>
+                </div>   <div className="menu-list">
+                    <li>
+                        <span className="material-symbols-outlined">Home </span>
+                        <p>Home</p>
+                    </li>
+                </div>
+                <div className="menu-list">
+                    <li>
+                        <span className="material-symbols-outlined">Home </span>
+                        <p>Home</p>
+                    </li>
+                </div>
 
+                <div className="menu-list">
+                    <li>
+                        <span className="material-symbols-outlined"> settings </span>
+                        <p>Configurações</p>
+                    </li>
+                </div>
+            </ul>
         </div>
     )
 }
