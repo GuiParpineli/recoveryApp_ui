0fa1e74d5bd153e841d869b6c86f42d2ebe8c1c5
Gui Parpineli <guiparpineli@GuiPC>
2022-12-31 17:13:20 -0300

    assert bar and grid in home layoyt

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,25 +3,21 @@ import './style.scss'
 export function Header() {
     return (
         <header className="header-main">
-            <img src="src/assets/logo.png"
-                 className="logo-header-home"
-                 alt=""/>
-            <input className="header-main-input" type="text"/>
-            <button className="search-button">
-                <span className="material-symbols-outlined">
-search
-</span>
-            </button>
+            <div className="logo-header-home">
+                <img src="src/assets/logo.png" alt=""/>
+            </div>
+            <div className="header-main-input">
+                <input type="text"/>
+                <button className="search-button">
+                    <span className="material-symbols-outlined black"> search </span>
+                </button>
+            </div>
             <div className="perfil-container">
                 <button className="perfil-button">
-                    <span className="material-symbols-outlined">
-person
-</span>
+                    <span className="material-symbols-outlined"> person </span>
                 </button>
                 <button className="logout-button">
-                    <span className="material-symbols-outlined">
-logout
-</span>
+                    <span className="material-symbols-outlined"> logout </span>
                 </button>
             </div>
         </header>
