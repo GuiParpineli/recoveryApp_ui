fe1bf33af083d98daac71441969005c4e2b6c099
Gui Parpineli <guiparpineli@GuiPC>
2022-12-31 00:42:34 -0300

    login page ui ok

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,13 +3,23 @@ import {FormLogin} from "../../components/Form-Login";
 
 export function Login() {
     return (
+
         <div className="main">
-            <div className="logo-container">
-                <img className="logo"
-                     src="src/assets/logo.png"
-                     alt="alluEmoji"/>
+
+            <div className="container-main">
+
+                <div className="logo-container">
+                    <img className="logo"
+                         src="src/assets/logo.png"
+                         alt="alluEmoji"/>
+                </div>
+                <FormLogin/>
             </div>
-            <FormLogin/>
+            <footer>
+                <p>
+                    Em caso de duvidas, entre em contato atraves do email exemplo@gmail.com
+                </p>
+            </footer>
         </div>
-)
+    )
 }
